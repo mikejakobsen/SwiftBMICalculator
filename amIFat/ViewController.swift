d9bc13f1fabc354d9b4fe2da599f6b820f9108e5
Mike Jakobsen <mike@jakobsen.dk>
2016-11-18 13:00:12 +0100

    Basic Function working. Can't reset though
    First run, works perfect. The second submit, dosen't get rendered.

--- amIFat/ViewController.swift
+++ amIFat/ViewController.swift
@@ -9,7 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // Result
+    @IBOutlet weak var bmiResult: UILabel!
+    @IBOutlet weak var weightOutlet: UILabel!
+    @IBOutlet weak var heightOutlet: UILabel!
+    @IBOutlet weak var fatLabel: UILabel!
+    
+    var h:Float = 1.6
+    var w:Float = 80
+    var bmi:Float = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,13 +29,37 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    @IBOutlet weak var weightInput: UITextField!
     
-    @IBOutlet weak var heightInput: UITextField!
-
-    @IBOutlet weak var calcBmi: UIButton!
+    // Knappen
+    @IBAction func amIFat(_ sender: Any) {
+        bmi = (w/(h*h))
+        
+        bmiResult.text = NSString(format: "%.2f", bmi) as String
+        
+        self.changeStatus(bmi: bmi)
+        
+    }
+    @IBAction func weightChanged(_ sender: UISlider) {
+        w = sender.value
+        weightOutlet.text = "\(w) kg"
+    }
+    @IBAction func heightChanged(_ sender: UISlider) {
+        h = sender.value
+        heightOutlet.text = "\(h) cm"
+    }
     
-    @IBOutlet weak var resultBmi: UILabel!
+    private func changeStatus(bmi: Float) {
+        if (bmi < 18.5) {
+            fatLabel.text = "Underweight"
+            fatLabel.textColor = UIColor.green
+        } else if (bmi >= 18.5 && bmi < 25) {
+            fatLabel.text = "Normal"
+            fatLabel.textColor = UIColor.yellow
+        } else {
+            fatLabel.text = "Overweight"
+            fatLabel.textColor = UIColor.red
+        
+        }
+    }
 }
 
